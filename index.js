54c15fed7ac90ee52c08a106f7693851e2be1c80
tanishqarora2600 <56479357+tanishqarora2600@users.noreply.github.com>
2021-10-01 16:28:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import { ApolloServer, gql } from "apollo-server-express";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./typeDefs";
-import cors from "cors";
 
 var admin = require("firebase-admin");
 
@@ -17,7 +16,6 @@ const isAuth = require("./middleware/isAuthenticated");
 
 const startServer = async () => {
   const app = express();
-  app.use(cors());
 
   app.use(isAuth);
   const server = new ApolloServer({
